cf515fe38d6488ea1fdcbd9c104cef258c126b17
Michael Utz <michaelutz@Michaels-MacBook-Pro.local>
2018-04-08 19:07:18 +0300

    chore: add site meta to index

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,31 +5,51 @@ import Helmet from 'react-helmet'
 import Header from 'components/Header'
 import './index.css'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+const TemplateWrapper = ({ children, data }) => {
+  const { title, description, keywords } = !!data && data.site.meta
+
+  return (
+    <div>
+      <Helmet
+        title={title}
+        meta={[
+          { name: 'description', content: description },
+          { name: 'keywords', content: keywords.join(', ') },
+        ]}
+      />
+      <Header />
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
+        }}
+      >
+        {children()}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
 }
+
+export const query = graphql`
+  query LayoutIndex {
+    ...SiteMetadata
+  }
+
+  fragment SiteMetadata on RootQueryType {
+    site {
+      meta: siteMetadata {
+        title
+        description
+        keywords
+      }
+    }
+  }
+`
 
 export default TemplateWrapper
