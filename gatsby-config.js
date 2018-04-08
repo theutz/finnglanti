6cb0fc1423f2848d69efb3768b6a01e7d5700bb6
Michael Utz <michaelutz@Michaels-MacBook-Pro.local>
2018-04-08 18:56:33 +0300

    chore: setup site meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Finnglanti',
+    description: 'Finnish for English speakers',
+    keywords: ['finnish', 'english', 'language']
   },
   plugins: ['gatsby-plugin-react-helmet'],
 };
